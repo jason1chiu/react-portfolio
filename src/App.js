364b5ef57b416915142d000a7201e3dd5121a1aa
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-05-06 19:43:35 -0400

    created the about me section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from './components/Header';
+import AboutMe from './components/AboutMe';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <AboutMe />
     </div>
   );
 }
